413dc406a0e574d88519a6c6e133b8f24a679bb2
Alexander Kostin <asurv2@gmail.com>
2022-10-19 17:38:26 +0300

    create file

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Login from "./LoginScreen/Login";
+import Register from "./RegistrationScreen/Registration";
+// import Home from "./screens/Home";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+    return (
+        <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    );
+};
