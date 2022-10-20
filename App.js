0c986e44cfb25d575e974c7f0d0ab3bf315f3d40
Alexander Kostin <asurv2@gmail.com>
2022-10-21 01:05:30 +0300

    add loginForm

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import LoginScreen from "./screens/auth/LoginScreen";
+import RegistrationScreen from "./screens/auth/RegistrationScreen";
 
 export default function App() {
     return (
         <>
+            {/* <RegistrationScreen /> */}
             <LoginScreen />
         </>
     );
