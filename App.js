b61ad7418b8f4d74d14c72c16a10ab9e92cd1487
Alexander Kostin <asurv2@gmail.com>
2022-10-23 16:49:41 +0300

    add file

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginScreen from "./screens/auth/LoginScreen";
+import RegistrationScreen from "./screens/auth/RegistrationScreen";
+
+export default function App() {
+    return (
+        <>
+            {/* <RegistrationScreen /> */}
+            <LoginScreen />
+        </>
+    );
+}
