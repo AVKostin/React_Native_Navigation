5d934d3b27d777dd8845ac95deab4517507e1ed1
Alexander Kostin <asurv2@gmail.com>
2022-10-24 19:46:04 +0300

    up

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import LoginScreen from "./screens/auth/LoginScreen";
 import RegistrationScreen from "./screens/auth/RegistrationScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AuthStack = createStackNavigator();
 
 export default function App() {
     return (
-        <>
-            {/* <RegistrationScreen /> */}
-            <LoginScreen />
-        </>
+        <AuthStack>
+            <NavigationContainer>
+                <AuthStack.Screen name="Login" component={LoginScreen} />
+                <AuthStack.Screen
+                    name="Register"
+                    component={RegistrationScreen}
+                />
+            </NavigationContainer>
+        </AuthStack>
     );
 }
