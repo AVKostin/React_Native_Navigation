9b6212ec9c155dc46abac212d832c8ffd05885dc
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-25 23:42:23 +0300

    update file

--- screens/auth/RegistrationScreen.js
+++ screens/auth/RegistrationScreen.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import {
-    StyleSheet,
     Text,
     View,
     ImageBackground,
     TextInput,
     TouchableOpacity,
-    Platform,
     Keyboard,
     TouchableWithoutFeedback,
-    Button,
 } from "react-native";
+
+import { styles } from "../../components/registerStyle";
 
 const initialState = {
     email: "",
@@ -102,13 +101,16 @@ export default function RegistrationScreen({ navigation }) {
                             </Text>
                         </TouchableOpacity>
                         <View style={styles.header}>
-                            <Button
+                            <TouchableOpacity
                                 onPress={() => navigation.navigate("Login")}
-                                title="Login"
-                            />
-                            <Text style={styles.headerEnter}>
-                                Уже есть аккаунт? Войти
-                            </Text>
+                                activeOpacity={0.5}
+                                style={styles.headerEnter}
+                            >
+                                <Text title="Login">
+                                    {" "}
+                                    Уже есть аккаунт? Войти
+                                </Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
                 </ImageBackground>
@@ -116,112 +118,3 @@ export default function RegistrationScreen({ navigation }) {
         </TouchableWithoutFeedback>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
-    image: {
-        flex: 1,
-        resizeMode: "cover",
-        justifyContent: "flex-end",
-    },
-    avatar: {
-        width: 120,
-        height: 120,
-        left: 134,
-        top: 203,
-        zIndex: 1,
-        backgroundColor: "#F6F6F6",
-        borderRadius: 16,
-    },
-    add: {
-        color: "#ff6c00",
-        width: 25,
-        height: 25,
-        left: 241,
-        top: 313,
-        zIndex: 2,
-        fontSize: 26,
-        textAlign: "center",
-        fontWeight: "200",
-        transform: [{ translateY: 18 }],
-    },
-    cicle: {
-        color: "#ff6c00",
-        width: 25,
-        height: 25,
-        left: 241,
-        top: 313,
-        zIndex: 2,
-        borderWidth: 1,
-        borderColor: "#ff6c00",
-        fontSize: 26,
-        borderRadius: 100,
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: "#f0f8ff",
-        height: 50,
-        borderRadius: 6,
-        fontColor: "#bdbdbd",
-        marginHorizontal: 16,
-        textAlign: "left",
-        paddingLeft: 16,
-        backgroundColor: "#eaeaea",
-    },
-    form: {
-        marginHorizontal: 0,
-        backgroundColor: "#ffffff",
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-        height: 549,
-        top: 150,
-        justifyContent: "flex-end",
-    },
-    inputTitle: {
-        // color: "#bdbdbd",
-        marginBottom: 16,
-        fontSize: 16,
-        marginLeft: 0,
-    },
-    btn: {
-        borderRadius: 100,
-        borderWidth: 1,
-        height: 51,
-        marginTop: 43,
-        justifyContent: "center",
-        alignItems: "center",
-        marginHorizontal: 16,
-        marginBottom: 16,
-        ...Platform.select({
-            ios: {
-                backgroundColor: "transparent",
-                borderColor: "#f0f8ff",
-            },
-            android: {
-                backgroundColor: "#ff6c00",
-                borderColor: "transparent",
-            },
-        }),
-    },
-    btnTitle: {
-        color: Platform.OS === "ios" ? "#ffa500" : "#f0f8ff",
-        fontSize: 16,
-    },
-    header: {
-        alignItems: "center",
-        marginBottom: 32,
-    },
-    headerTitle: {
-        fontSize: 30,
-        fontWeight: "bold",
-        color: "#212121",
-    },
-    headerEnter: {
-        fontSize: 18,
-        color: "#1B4371",
-        marginBottom: 45,
-    },
-});
